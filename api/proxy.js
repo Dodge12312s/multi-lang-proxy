87594660a8816d036f39068cc93dc8f29926e7da
David Zimenkov <youmaycallmedave@gmail.com>
2025-10-29 15:38:37 +0100

    Correct v1

--- api/proxy.js
+++ api/proxy.js
@@ -1,10 +1,3 @@
-export const config = { runtime: 'edge' };
-
-export default async function handler(request) {
-  return new Response("hello world321123", {
-    status: 200,
-    headers: {
-      "content-type": "text/plain"
-    }
-  });
+export default function handler(req, res) {
+  res.status(200).send('Hello World');
 }
