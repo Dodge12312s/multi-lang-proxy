629675fb6af958f33404eb8ac67e5d6e74182cf3
David Zimenkov <youmaycallmedave@gmail.com>
2025-10-29 15:34:07 +0100

    Correct name of numb

--- api/proxy.js
+++ api/proxy.js
@@ -1,7 +1,7 @@
 export const config = { runtime: 'edge' };
 
 export default async function handler(request) {
-  return new Response("hello world321", {
+  return new Response("hello world321123", {
     status: 200,
     headers: {
       "content-type": "text/plain"
